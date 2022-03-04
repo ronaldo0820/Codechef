f98cb348b82940e95d1829365ff4a29c5fabbd61
RONALDO MATTHEW J R <95460329+ronaldo0820@users.noreply.github.com>
2022-03-04 13:59:16 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- >0){
+		    int c =0;
+		    int n = sc.nextInt();
+		    for(int i = 0; i<n; i++){
+		        int x = sc.nextInt();
+		        if(x%2 == 1){
+		            c++;
+		        }
+		    }if(c%2==1){
+		        c-=1;
+		    }
+		    System.out.println(c/2);
+		}
+	}
+}
